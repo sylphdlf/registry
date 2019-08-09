00916b44104f7b9bbdd1e51d33664aa701e70618
sylph <sylphdlf@163.com>
2019-08-09 16:36:54 +0800

    创建

---
+++ src/main/java/com/dlf/RegistryApplication.java
@@ -0,0 +1,20 @@
+package com.dlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
