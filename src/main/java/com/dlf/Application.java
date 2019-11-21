7e33e4cf98aee17c2a55e96fee5843cb43f611c2
sylph <sylphdlf@163.com>
2019-11-21 15:18:39 +0800

    代码优化

--- src/main/java/com/dlf/Application.java
+++ src/main/java/com/dlf/Application.java
@@ -3,6 +3,9 @@ package com.dlf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -15,5 +18,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
